3f13ed49b597260e09fcedc4d4e594e47be087bf
Rituraj Borpujari <riturajborpujari@outlook.com>
2018-11-29 19:40:02 +0530

    added the front end templates

---
+++ templates/include/footer.php
@@ -0,0 +1,7 @@
+<div id="footer">
+        Blog of Rituraj &copy; 2011. All rights reserved. <a href="admin.php">Site Admin</a>
+      </div>
+ 
+    </div>
+  </body>
+</html>
---
+++ templates/include/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title><?php echo htmlspecialchars( $results['pageTitle'] )?></title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+  </head>
+  <body>
+    <div id="container">
+ 
+      <a href="."><img id="logo" src="images/logo.jpg" alt="Blog Of Rituraj" /></a>
