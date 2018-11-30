8bf9c7f6baead2086b81a083c2d4f72c7fd23158
Rituraj Borpujari <riturajborpujari@outlook.com>
2018-11-30 14:07:07 +0530

    added table schema for comments

--- tables.sql
+++ tables.sql
@@ -1,12 +1,21 @@
-DROP TABLE IF EXISTS articles;
 
-CREATE TABLE articles
+CREATE TABLE IF NOT EXISTS articles
 (
-	id			smallint unsigned NOT NULL auto_increment,
+	id					smallint unsigned NOT NULL auto_increment,
 	publicationDate		date NOT NULL,
-	title			varchar(255) NOT NULL,
-	summary			text NOT NULL,
-	content			mediumtext NOT NULL,
+	title				varchar(255) NOT NULL,
+	summary				text NOT NULL,
+	content				mediumtext NOT NULL,
 
 	PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS comments
+(
+	id					smallint unsigned NOT NULL auto_increment,
+	publicationDate		date NOT NULL,
+	username			varchar(30) NOT NULL,
+	comment				text NOT NULL,
+	articleId			smallint unsigned NOT NULL,
+	PRIMARY KEY (id, articleId)
 );
