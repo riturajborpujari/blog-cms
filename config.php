c854b8e90543feafc683b8178bbac3befb4d7693
Rituraj Borpujari <riturajborpujari@outlook.com>
2018-12-03 15:02:09 +0530

    included the Comment.php file

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 ini_set( "display_errors", true);
 date_default_timezone_set("Asia/Kolkata");
 
-define( "DB_DSN", "mysql:host=<sql_server_host>;dbname=<sql_database>" );
+define( "DB_DSN", "mysql:host=localhost;dbname=cms" );
 define( "DB_USERNAME", "" );
 define( "DB_PASSWORD", "" );
 define( "CLASS_PATH", "classes" );
@@ -13,10 +13,11 @@ define( "ADMIN_USERNAME", "" );
 define( "ADMIN_PASSWORD", "" );
 
 require( CLASS_PATH . "/Article.php" );
+require( CLASS_PATH . "/Comment.php" );
 
 function handleException( $exception )
 {
-	echo "Sorry! An error occured.";
+	echo "Sorry! An error occured." . $exception;
 	error_log( $exception->getMessage() );
 }
 
